889562989e849c74110d5ef2e09a25a539a8433f
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-08-17 01:19:21 +0530

    Solved problems for 34th day

---
+++ Graph/DetectCycleInAnUndirectedGraph.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+class Solution {
+  private:
+   bool dfs(int node, int parent, vector<int>& vis, vector<int> adj[]) {
+       vis[node] = 1;
+       for (auto adjacentNode : adj[node]) {
+           if (!vis[adjacentNode]) {
+               if (dfs(adjacentNode, node, vis, adj) == true)
+               return true;
+           }
+           else if (adjacentNode != parent) return true;
+       }
+       return false;
+   }
+  public:
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> vis(V, 0);
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (dfs(i, -1, vis, adj) == true) return true;
+            } 
+        }
+        return false;
+    }
+};
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
